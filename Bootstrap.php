b5fee2ed7df3ede978d406768145cddadd97996a
Mike Netz <netz@arvatis.com>
2016-05-31 15:12:57 +0200

    version fix

--- Bootstrap.php
+++ Bootstrap.php
@@ -14,7 +14,7 @@ class Shopware_Plugins_Frontend_ArvGoogleRemarketing_Bootstrap extends Shopware_
      */
     public function getVersion()
     {
-        return '1.0.3';
+        return '1.0.4';
     }
 
     /**

652de3c566ae2d012e817689282c8ed6e92ff322
Jochen Manz <jochen.manz@gmx.de>
2017-01-23 10:27:42 +0100

    bump plugin version to enforce that shopware will provide the plugin unencoded

--- Bootstrap.php
+++ Bootstrap.php
@@ -29,7 +29,7 @@ class Shopware_Plugins_Frontend_ArvGoogleRemarketing_Bootstrap extends Shopware_
      */
     public function getVersion()
     {
-        return '1.0.4';
+        return '1.0.5';
     }
 
     /**
